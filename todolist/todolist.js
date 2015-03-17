34e2ec3b8553312a731935ee177eef194489f531
Kevin Keller <flygeneticist@gmail.com>
2015-03-17 12:30:42 +0000

    Mongo hookup added for lists, users, and tasks.

--- todolist/todolist.js
+++ todolist/todolist.js
@@ -1,15 +1,15 @@
+Users = new Mongo.Collection("users");
+Lists = new Mongo.Collection("lists");
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    tasks: [
-      { text: "This is task 1" },
-      { text: "This is task 2" },
-      { text: "This is task 3" }
-    ],
-    lists: [
-      {title: 'Title Test', dueDate: '03/16/2015'},
-      {title: '^__^', dueDate: '03/20/2015'},
-      {title: 'More lists!!', dueDate: '03/20/2015'}
-    ]
+    lists: function () {
+      return Lists.find({});
+    },
+    tasks: function () {
+      return Tasks.find({});
+    }
   });
 }

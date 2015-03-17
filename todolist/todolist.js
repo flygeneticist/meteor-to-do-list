1d41dfcae4dc71dca9b299ef5a15cadbcdfae583
Kevin Keller <flygeneticist@gmail.com>
2015-03-17 13:30:58 +0000

    delete and add lists functioning now.

--- todolist/todolist.js
+++ todolist/todolist.js
@@ -12,4 +12,25 @@ if (Meteor.isClient) {
       return Tasks.find({});
     }
   });
+
+  Template.body.events({
+    "submit .new-list": function (event) {
+      // This function is called when the new task form is submitted
+      var title = event.target.title.value;
+
+      Lists.insert({
+        title: title,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.title.value = "";
+      // Prevent default form submit
+      return false;
+    },
+
+    "click .deleteListBtn": function () {
+      Lists.remove(this._id);
+    }
+  });
 }
